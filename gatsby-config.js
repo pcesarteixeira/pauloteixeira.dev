915750ff142f6a0e5b93ec0640b26a316368b6de
Paulo Teixeira <dev.paulocesar@gmail.com>
2019-08-08 19:44:39 -0300

    tracking id google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-136342699-1`,
       },
     },
     `gatsby-plugin-feed`,

bc701cdeeafc6042a9eebb69f118c510576e094b
Paulo César Teixeira <dev.paulocesar@gmail.com>
2020-04-28 19:24:01 -0300

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     }
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     'gatsby-plugin-robots-txt',
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/404.js
+++ src/pages/404.js
@@ -12,8 +12,7 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>404: Not Found</h1>
       </Layout>
     )
   }
